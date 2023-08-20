4f2c3048ffdf8148d82b4dfab8cbdcaebd40d8b9
helmogey <h.elmogey@gmail.com>
2023-08-20 21:19:08 +0300

    printf

---
+++ _printf.c
@@ -0,0 +1,48 @@
+
+#include <stdarg.h>
+/**
+*print_all - start
+*Description: 'print string'
+*@format: sep
+*Return: void
+*/
+int _printf(const char *format, ...)
+{
+int i = 0;
+char *str, *sep = "";
+
+va_list list;
+va_start(list, format);
+if (format)
+{
+while (format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(list, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(list, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(list, double));
+break;
+case 's':
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
+}
+sep = ", ";
+i++;
+}
+}
+printf("\n");
+va_end(list);
+return (39);
+}
