9d855fb959d01bffb483dc02a7b9c851d6c99762
helmogey <h.elmogey@gmail.com>
2023-08-21 14:13:36 +0300

    printf

--- main.h
+++ main.h
@@ -1,1 +1,7 @@
+#include <stdarg.h>
+
+
 int _printf(const char *format, ...);
+int print_char(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size
