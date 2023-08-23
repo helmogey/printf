f109594e531fb6f49458ab0c9a94b1666e27df59
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-23 05:44:05 +0300

    scnd_tsting

--- helper_funcs.c
+++ helper_funcs.c
@@ -19,3 +19,12 @@ long int convert_size_number(long int num, int size)
 
 	return ((int)num);
 }
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
--- main.h
+++ main.h
@@ -47,4 +47,4 @@ int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag
 int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
-
+long int convert_size_unsgnd(unsigned long int num, int size);
